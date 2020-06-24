5e68a3173d227c43ee2f9eeb304bd635986025d0
Vivekchandrah <vivekchandrah@imsspl.com>
2020-06-24 15:41:39 +0530

    Quiz code

---
+++ src/components/Quiz.js
@@ -0,0 +1,80 @@
+import React,{ Component } from 'react';
+import PropTypes from 'prop-types';
+import { CSSTransitionGroup } from 'react-transition-group';
+import Question from '../components/Question';
+import QuestionCount from '../components/QuestionCount';
+import AnswerOption from '../components/AnswerOption';
+
+class Quiz extends Component {
+
+   renderAnswerOptions=(key)=> {
+    return (
+      <AnswerOption
+        key={key.content}
+        answerContent={key.content}
+        answerType={key.type}
+        answer={this.props.answer}
+        questionId={this.props.questionId}
+        onAnswerSelected={this.props.onAnswerSelected}
+      />
+    );
+  }
+
+  render (){
+    return (
+      <CSSTransitionGroup
+        className="container"
+        component="div"
+        transitionName="fade"
+        transitionEnterTimeout={800}
+        transitionLeaveTimeout={500}
+        transitionAppear
+        transitionAppearTimeout={500}
+      >
+        <div key={this.props.questionId}>
+          <QuestionCount counter={this.props.questionId} total={this.props.questionTotal} />
+          <Question content={this.props.question} />
+          <ul className="answerOptions">
+            {this.props.answerOptions && this.props.answerOptions.map(this.renderAnswerOptions)}
+          </ul>
+          <div style={{marginLeft:'30%' ,padding: '5px',}}>
+          <button style={{color: 'blue', padding: '5px', borderRadius: '15px',marginRight: '5px'}}
+          disabled={this.props.questionId===1 ? true : false} onClick={()=>this.props.back()} >
+            Pervious
+          </button> 
+          <button
+          style={{color: 'blue', padding: '5px', borderRadius: '15px',marginRight: '5px'}}
+           disabled={this.props.questionId===this.props.questionTotal ? true : false} onClick={()=>this.props.next()}>
+            Next
+          </button>
+          <button 
+          style={{color: 'Red', padding: '5px', borderRadius: '15px',marginRight: '5px'}}
+          onClick={()=>this.props.submit()}>
+            Submit
+          </button>
+          <button 
+          style={{color: 'green', padding: '5px', borderRadius: '15px',marginRight: '5px'}}
+          onClick={()=>this.props.showAnswer()}
+          >
+            Show answer
+          </button>
+          </div>
+        </div>
+       </CSSTransitionGroup>
+    );
+  }
+  
+}
+
+Quiz.propTypes = {
+  answer: PropTypes.string.isRequired,
+  answerOptions: PropTypes.array.isRequired,
+  question: PropTypes.string.isRequired,
+  questionId: PropTypes.number.isRequired,
+  questionTotal: PropTypes.number.isRequired,
+  onAnswerSelected: PropTypes.func.isRequired,
+  back:PropTypes.func.isRequired,
+  next:PropTypes.func.isRequired,
+};
+
+export default Quiz;
---
+++ src/config/quizQuestions.js
@@ -0,0 +1,93 @@
+var quizQuestions = [
+  {
+      id: 1,
+      question: "Who won 2011 worldcup ?",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "India"
+          },
+          {
+              type: "Nintendo",
+              content: "Australia"
+          },
+          {
+              type: "Sony",
+              content: "South africa"
+          },{
+            type: "Sony",
+            content: "West indies"
+        }
+      ],
+      correctAnswer: 'India'
+  },
+  {
+      id: 2,
+      question: "What is the current year?",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "2000"
+          },
+          {
+              type: "Nintendo",
+              content: "2010"
+          },
+          {
+              type: "Sony",
+              content: "2020"
+          },{
+            type: "Sony",
+            content: "2030"
+        }
+      ],
+      correctAnswer: '2020'
+  },
+  {
+      id: 3,
+      question: "what is the current month?",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "January"
+          },
+          {
+              type: "Nintendo",
+              content: "August"
+          },
+          {
+              type: "Sony",
+              content: "March"
+          },{
+            type: "Sony",
+            content: "December"
+        }
+      ],
+      correctAnswer: 'March'
+  },
+  {
+      id:4,
+      question: "Who is the Ronaldo",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "Football player"
+          },
+          {
+              type: "Nintendo",
+              content: "Crickter"
+          },
+          {
+              type: "Sony",
+              content: "Tennis player"
+          },{
+            type: "Sony",
+            content: "Rugby player"
+        },
+
+      ],
+      correctAnswer: 'Football player'
+  },
+];
+
+export default quizQuestions;
